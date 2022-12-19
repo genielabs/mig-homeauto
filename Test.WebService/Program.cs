401320c3cc4e7d68f9af7783a13c7b7ed3fab09d
Gene <genemars@users.noreply.github.com>
2022-12-19 19:06:48 +0100

    Upgraded to .NET 6

--- Test.WebService/Program.cs
+++ Test.WebService/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*
   This file is part of MIG (https://github.com/genielabs/mig-service-dotnet)
  
-  Copyright (2012-2018) G-Labs (https://github.com/genielabs)
+  Copyright (2012-2023) G-Labs (https://github.com/genielabs)
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -16,9 +16,6 @@
   limitations under the License.
 */
 
-using System;
-using System.IO;
-using System.Threading;
 using System.Xml.Serialization;
 
 using MIG;
